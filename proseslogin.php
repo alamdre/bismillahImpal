11c74cf729a7ace4a64d11e8731a2b656ab1fc95
alamdre <aka.alam97@gmail.com>
2017-09-06 21:31:58 +0700

    Add files via upload

---
+++ proseslogin.php
@@ -0,0 +1,24 @@
+ <?php
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password     = $_POST['password'];
+
+$login = mysqli_query($connect, "SELECT * FROM dbakun WHERE username = '$username' AND password='$password'");
+$row=mysqli_fetch_array($login);
+if ($row['username'] == $username AND $row['password'] == $password)
+{
+  session_start();
+  $_SESSION['username'] = $row['username'];
+  $_SESSION['password'] = $row['password'];
+  header('location:index.html');
+}
+else
+{
+  echo "<center><br><br><br><br><br><br><b>LOGIN GAGAL! </b><br>
+        Username atau Password Anda tidak benar.<br>";
+    echo "<br>";
+  echo "<input class='btn btn-blue' type=button value='ULANGI LAGI' onclick=location.href='login.html'></a></center>";
+
+}
+?>
